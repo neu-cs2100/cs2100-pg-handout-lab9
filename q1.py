d4ed18297cb22f2e54988e2d52d3bc286bdec476
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-08-14 17:46:20 -0400

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,9 +1,10 @@
+"""
+Please add methods to the Coordinates class to make it possible to compare Coordinates.
+A Coordinates object is "smaller" if it is closer to your favorite city.
+Test that the comparison works the way you want.
+"""
 
 class Coordinates:
     def __init__(self, latitude: float, longitude: float):
         # Save the latitude and longitude to attributes
         pass
-
-    # Add methods to make it possible to compare Coordinates.
-    # A Coordinates object is "smaller" if it is closer to your favorite city.
-    # Test that the comparison works the way you want.
